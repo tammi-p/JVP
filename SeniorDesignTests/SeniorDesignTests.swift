3f2023ece9fbccef00e464bc4b7611fb4f1d2d19
alvinmagee <awm5@rice.edu>
2020-02-18 20:44:50 -0600

    Initial Commit

---
+++ SeniorDesignTests/SeniorDesignTests.swift
@@ -0,0 +1,34 @@
+//
+//  SeniorDesignTests.swift
+//  SeniorDesignTests
+//
+//  Created by Alvin Magee on 2/18/20.
+//  Copyright © 2020 Alvin Magee. All rights reserved.
+//
+
+import XCTest
+@testable import SeniorDesign
+
+class SeniorDesignTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
